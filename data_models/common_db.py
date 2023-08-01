0385624f86195ab46cb165de9637903b054bb49d
Yijun Lin <linyijun@users.noreply.github.com>
2023-08-02 00:45:03 +0800

    Update common_db.py

--- data_models/common_db.py
+++ data_models/common_db.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-connection_string = 'postgresql+psycopg2://{usr}:{pwd}@jonsnow.usc.edu/air_quality_dev' \
-    .format(usr='yijun', pwd='m\\tC7;cc')
+connection_string = 'postgresql+psycopg2://{usr}:{pwd}@{server}/air_quality_dev' \
+    .format(usr='', pwd='')
 
 engine = create_engine(connection_string, echo=False)
 
